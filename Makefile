d08a53c5a1b44cb4bfafe4e0583868e4258471ec
jthoms29 <johnthoms567@gmail.com>
2024-12-08 17:19:31 -0600

    Can now read flac metadata using taglib

--- Makefile
+++ Makefile
@@ -2,6 +2,11 @@ CC = gcc
 CGLAGS = -g
 CPPFLAGS = -Wall -pedantic -Wextra
 
+TAGLIB = /usr/local/lib/
+#libtag_c.a
+TAGINCLUDE = /usr/local/include/taglib/
+
+
 OBJ_DIR=build/$(PLATFORM)/obj
 LIB_DIR=build/$(PLATFORM)/lib
 BIN_DIR=build/$(PLATFORM)/bin
@@ -26,12 +31,11 @@ $(BIN_DIR):
 
 
 $(OBJ_DIR)/main.o : main.c | $(OBJ_DIR)
-	$(CC) -c $(CFLAGS) $(CPPFLAGS) main.c -I. -o $(OBJ_DIR)/main.o
-
+	$(CC) -c $(CFLAGS) $(CPPFLAGS) main.c -I. -I$(TAGINCLUDE) -o $(OBJ_DIR)/main.o
 
 $(BIN_DIR)/music_player : $(OBJ_DIR)/main.o | $(BIN_DIR)
-	$(CC) $(CFLAGS) $(OBJ_DIR)/main.o -o $(BIN_DIR)/music_player
-
+	$(CC) $(CFLAGS) -L$(TAGLIB) $(OBJ_DIR)/main.o -ldl -lpthread -lm -ltag_c -ltag -lstdc++ -lz -o \
+	$(BIN_DIR)/music_player
 
 music_player : $(BIN_DIR)/music_player
 	ln -sf $(BIN_DIR)/music_player music_player
--- main.c
+++ main.c
@@ -2,10 +2,14 @@
 #include <music_defs.h>
 #include <pthread.h>
 #include <string.h>
+#include <tag_c.h>
+
 
 #define MINIAUDIO_IMPLEMENTATION
 #include <miniaudio.h>
 
+
+/* TODO wip, shouldn't loop forever */
 ma_result miniaudio_init(char* path) {
   ma_result result;
   ma_engine engine;
@@ -15,17 +19,33 @@ ma_result miniaudio_init(char* path) {
     return result;
   }
 
-
   ma_engine_play_sound(&engine, path, NULL);
   for (;;) {}
 
+}
 
+void read_tag(char* path) {
+  TagLib_File *file;
+  TagLib_Tag *tag;
+
+  file = taglib_file_new(path);
+  tag = taglib_file_tag(file);
+
+  printf("%s\n", taglib_tag_title(tag));
+
+  printf("%s\n", taglib_tag_artist(tag));
+
+  printf("%s\n", taglib_tag_album(tag));
+  printf("%d\n", taglib_tag_year(tag));
+  printf("%d\n", taglib_tag_track(tag));
+  printf("%s\n", taglib_tag_genre(tag));
 }
 
 int main(int argc, char** argv) {
  char path[256];
  strcpy(path, argv[1]);
  printf("%s\n", path);
+ read_tag(path);
  miniaudio_init(path);
 
 }
