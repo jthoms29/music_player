4b0d7b3e26e4d85077179c0c70dc4ffb518a02d2
jthoms29 <johnthoms567@gmail.com>
2024-12-26 20:55:41 -0600

    Implementing thread synchronization with mutexes

--- main.c
+++ main.c
@@ -7,6 +7,10 @@
 #include <miniaudio.h>
 
 
+pthread_mutex_t cursor_sleep = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t control_sleep = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t control_cmd = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t player_sleep = PTHREAD_MUTEX_INITIALIZER;
 /* The music library will be held in this array, which is indexed by the ascii
  * code of the first letter of the artist's name. Each contains a linked list
  * of artists, each of which contains a linked list of albums */
@@ -16,8 +20,11 @@
 GList library[81];
 
 ma_engine* engine;
-int paused;
+ma_sound sound;
+char command[256];
+int cmd_alert;
 
+int paused;
 
 ma_result init_engine() {
   ma_result result;
@@ -28,116 +35,100 @@ ma_result init_engine() {
   return result;
 }
 
-/* plays specified album, starting at specified track number */
-int play_audio(album* albm, int start_track) {
-  int total_tracks, i;
-  int* track_ref;
-  GList* song_walker;
-  song* cur_song;
-  song_walker = albm->songs;
+int play_audio(GList* songs) {
   ma_result result;
-  ma_sound sound;
-  char command[256];
+  song* cur_song;
 
-  paused = 0;
-  track_ref = &start_track;
-  song_walker = g_list_find_custom(albm->songs, (gconstpointer) track_ref,
-    (GCompareFunc) find_track);
-
-  if (!song_walker) {
-    printf("Invalid track\n");
-    return -1;
-  }
-
-  while (song_walker) {
-    cur_song = (song*) song_walker->data;
+  while(songs != NULL) {
+    cur_song = (song*) songs->data;
     result = ma_sound_init_from_file(engine, cur_song->path, 0, NULL, NULL,
       &sound);
     if (result != MA_SUCCESS) {
       return result;
     }
 
-//    ma_sound_set_stop_time_in_milliseconds(&sound, 1);
-    ma_sound_start(&sound);
-
-    while (!ma_sound_at_end(&sound)) {
-
+    while(!ma_sound_at_end(&sound)) {
       /* refresh line */
       printf("\r                                                           "
         "                                                                  ");
-      printf("\r%s - %s: %d - %s    command:  ", albm->artist, albm->title,
-        cur_song->track, cur_song->title);
+      printf("\r%s - %s: %d - %s    command:  ", cur_song->artist, 
+        cur_song->album, cur_song->track, cur_song->title);
       fflush(stdout);
-      fgets(command, 256, stdin);
 
-      command[strlen(command) - 1] = 0;
-      if (strcmp(command, "next") == 0 || !strcmp(command, "prev")) {
-        break;
-      }
-      if (!strcmp(command, "pause") && !paused) {
+      if(cmd_alert) {
+
+        if (strcmp(command, "next") == 0 || !strcmp(command, "prev")) {
+          break;
+        }
+
+        if (!strcmp(command, "pause") && !paused) {
           ma_sound_stop(&sound);
           paused = 1;
+        }
+
+        if (!strcmp(command, "unpause") && paused) {
+          paused = 0;
+          ma_sound_start(&sound);
+        }
+
+        cmd_alert = 0;
+        pthread_mutex_unlock(&control_cmd);
+      }
+    }
+
+    if (cmd_alert) {
+      if (!strcmp(command, "prev")) {
+        songs = songs->prev;
       }
 
-      if (!strcmp(command, "unpause") && paused) {
-        ma_sound_start(&sound);
+      if (!strcmp(command, "next")) {
         paused = 0;
+        songs = songs->next;
       }
+      cmd_alert = 0;
+      pthread_mutex_unlock(&control_cmd);
     }
 
-    if (!paused)
-      ma_sound_stop(&sound);
+    else {
+      /* default functionality is to automatically play next song in album */
+      songs = songs->next;
+    }
 
+    ma_sound_stop(&sound);
     ma_sound_uninit(&sound);
-    
-    if (!strcmp(command, "next")) {
-      paused = 0;
-      song_walker = song_walker->next;
-
-    }
-
-    if (!strcmp(command, "prev"))
-        song_walker = song_walker->prev;
 
   }
-
 }
 
-/* switches between library and song controls. Audio should always play
- * in backround except if paused. */
-void server(void) {
-  char message[256];
+/* plays specified album, starting at specified track number */
+int controls(album* albm, int start_track) {
+
+  for(;;) {
+    fgets(command, 256, stdin);
+
+    command[strlen(command) - 1] = 0;
+    cmd_alert = 1;
+    pthread_mutex_lock(&control_cmd);
+  }
+}
+
+
+int main(int argc, char** argv) {
+  char* path;
   pthread_t thread_table[3];
+  int i;
 
-
+  path = (char*) malloc(strlen(argv[1]));
+  strcpy(path, argv[1]);
+  init_engine();
+  scan_folder(path);
   pthread_create(thread_table, NULL, cursor, NULL);
   pthread_create(thread_table+1, NULL, play_audio, NULL);
   pthread_create(thread_table+2, NULL, controls, NULL);
 
-
-  for(;;) {
-
-
+  for(i=0; i < 3; i++) {
+    pthread_join(thread_table[i], NULL);
   }
 
-
-
-
-
-
-
-
-}
-
-int main(int argc, char** argv) {
- char* path;
-
- path = (char*) malloc(strlen(argv[1]));
- strcpy(path, argv[1]);
- init_engine();
- scan_folder(path);
- server();
-
-
- return 0;
+  return 0;
 }
